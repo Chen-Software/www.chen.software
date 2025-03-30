116c39f0f4800ad6e5f0bf25048c30971a174c70
Yumin Chen <yumin@chen.so>
2025-03-30 08:29:09 +0100

    Use Heading component
    - Issue: https://git.chen.so/tina-next-ts/i/42cbf04e3f925771cd03e661b72406a0aa125ef9

--- app/posts/[...filename]/client-page.tsx
+++ app/posts/[...filename]/client-page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { H1 } from "app/components/Heading";
 import { tinaField, useTina } from "tinacms/dist/react";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 import { Box, Flex } from "../../../styled-system/jsx";
@@ -31,9 +32,9 @@ export default function Post(props: ClientPageProps) {
 				<PostList data={props.postListData} sidebarMode={true} />
 			</Box>
 			<Box>
-				<h1 data-tina-field={tinaField(data.post, "title")}>
+				<H1 data-tina-field={tinaField(data.post, "title")}>
 					{data.post.title}
-				</h1>
+				</H1>
 				<Box data-tina-field={tinaField(data.post, "body")}>
 					<TinaMarkdown content={content} />
 				</Box>
