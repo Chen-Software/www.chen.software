5ccf478cdc5f1e346682d270625b53ff01df343a
Yumin Chen <yumin@chen.so>
2025-03-31 14:52:53 +0100

    Breadcrumbs component
    - Issue: https://git.chen.so/tina-next-ts/i/c5e14d1619ab76a8b788b51e06219e339b85f7a9

---
+++ app/components/Breadcrumbs/index.tsx
@@ -0,0 +1,33 @@
+import React, { createElement as h } from "react";
+import { Box, Flex } from "styled-system/jsx";
+
+export const Breadcrumbs = ({ children, ...props }) => {
+	return (
+		<Box as="nav" aria-label="breadcrumb" cursor="pointer">
+			<Flex
+				as="ol"
+				display="flex"
+				listStyleType="none"
+				alignItems="center"
+				padding={0}
+				margin={0}
+				{...props}
+			>
+				<Box as="span" marginRight={3} marginBottom={1.5} fontSize="3xl">
+					☰
+				</Box>
+
+				{React.Children.map(children, (child, index) => (
+					<>
+						{child}
+						{index !== React.Children.count(children) - 1 && (
+							<Box as="span" marginX={3}>
+								{"➔"}
+							</Box>
+						)}
+					</>
+				))}
+			</Flex>
+		</Box>
+	);
+};
