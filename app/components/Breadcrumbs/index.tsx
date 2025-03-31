49563f3b1f5921b40dba34a3087fcff4099ca781
Yumin Chen <yumin@chen.so>
2025-03-31 20:22:18 +0100

    Force Breadcrumbs in one line with `whiteSpace="nowrap"`
    - Issue: https://git.chen.so/tina-next-ts/i/c5e14d1619ab76a8b788b51e06219e339b85f7a9

--- app/components/Breadcrumbs/index.tsx
+++ app/components/Breadcrumbs/index.tsx
@@ -6,11 +6,11 @@ export const Breadcrumbs = ({ children, ...props }) => {
 		<Box as="nav" aria-label="breadcrumb" cursor="pointer">
 			<Flex
 				as="ol"
-				display="flex"
 				listStyleType="none"
 				alignItems="center"
 				padding={0}
 				margin={0}
+				whiteSpace="nowrap"
 				{...props}
 			>
 				<Box as="span" marginRight={3} marginBottom={1.5} fontSize="3xl">
