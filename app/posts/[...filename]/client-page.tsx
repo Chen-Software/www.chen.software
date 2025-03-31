252fda81ed8cd0d17ea3d4e7eb83328a1f12041e
Yumin Chen <yumin@chen.so>
2025-03-31 14:58:02 +0100

    Add Breadcrumbs to the post page
    - Issue: https://git.chen.so/tina-next-ts/i/c5e14d1619ab76a8b788b51e06219e339b85f7a9

--- app/posts/[...filename]/client-page.tsx
+++ app/posts/[...filename]/client-page.tsx
@@ -1,7 +1,11 @@
 "use client";
+import { Breadcrumbs } from "app/components/Breadcrumbs";
+import { Button } from "app/components/Button";
 import { Code } from "app/components/Code";
+import { Drawer } from "app/components/Drawer";
 import { H1, H2, H3, H4, H5, H6 } from "app/components/Heading";
 import { Paragraph } from "app/components/Text";
+import Link from "next/link";
 import { tinaField, useTina } from "tinacms/dist/react";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 import { Box, Flex } from "../../../styled-system/jsx";
@@ -20,39 +24,63 @@ interface ClientPageProps {
 	postListData: PostConnectionQuery;
 }
 
-export default function Post(props: ClientPageProps) {
+export default function Post({ postListData, ...props }: ClientPageProps) {
 	// data passes though in production mode and data is updated to the sidebar data in edit-mode
 	const { data } = useTina({
 		query: props.query,
 		variables: props.variables,
 		data: props.data,
 	});
+
 	const content = data.post.body;
+	const postList = <PostList data={postListData} sidebarMode={true} />;
+	const breadcrumbs = (
+		<Breadcrumbs>
+			<Link href="/posts">Post</Link>
+			<Link href={`/posts/${data.post._sys.filename}`}>{data.post.title}</Link>
+		</Breadcrumbs>
+	);
+
 	return (
-		<Flex flexDirection="row" gap={4}>
-			<Box minWidth={["128px", "196px", "256px", "320px", "384px"]}>
-				<PostList data={props.postListData} sidebarMode={true} />
-			</Box>
-			<Box>
-				<H1 data-tina-field={tinaField(data.post, "title")}>
-					{data.post.title}
-				</H1>
-				<Box data-tina-field={tinaField(data.post, "body")}>
-					<TinaMarkdown
-						components={{
-							h1: H1,
-							h2: H2,
-							h3: H3,
-							h4: H4,
-							h5: H5,
-							h6: H6,
-							p: Paragraph,
-							code: Code,
-						}}
-						content={content}
-					/>
+		<>
+			<Box hideBelow="md">{breadcrumbs}</Box>
+			<Flex flexDirection="row" gap={4}>
+				<Box
+					hideBelow="md"
+					minWidth={["128px", "196px", "256px", "320px", "384px"]}
+				>
+					{postList}
 				</Box>
-			</Box>
-		</Flex>
+				<Box>
+					<Box hideFrom="md">
+						<Drawer
+							variant="left"
+							title="Posts"
+							trigger={breadcrumbs}
+							content={postList}
+							footer={false}
+						/>
+					</Box>
+					<H1 data-tina-field={tinaField(data.post, "title")}>
+						{data.post.title}
+					</H1>
+					<Box data-tina-field={tinaField(data.post, "body")}>
+						<TinaMarkdown
+							components={{
+								h1: H1,
+								h2: H2,
+								h3: H3,
+								h4: H4,
+								h5: H5,
+								h6: H6,
+								p: Paragraph,
+								code: Code,
+							}}
+							content={content}
+						/>
+					</Box>
+				</Box>
+			</Flex>
+		</>
 	);
 }
